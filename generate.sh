068c0db902d354ab98a3a79ede4f94cc7c83910c
AdryDev <adrydevmateo@gmail.com>
2024-11-20 00:45:08 -0400

    Initial Commit

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Generates a new Eleventy project
+
+# Variables
+declare PROJECT_NAME
+declare PACKAGE_MANAGER
+declare IS_BASIC
+
+# 1. Ask for the project name
+# 1.1 Check if the project already exists
+# 1.2 Create the project directory
+
+# 2. Ask for the package manager
+
+# 3. Ask if it's a basic project
